cf26e1837ca32370fb78baee36172beaa4204438
Mili Wagnerová <wagnerovamili@gmail.com>
2021-04-24 12:09:47 +0200

    Úprava rozbalovacího menu

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import './style.css';
 console.log('funguju!');
 
 const nav = document.querySelector('#nav-btn');
+const navigation = document.querySelector('nav');
+const allItems = document.querySelectorAll('.nav_link');
 
 nav.addEventListener('click', (e) => {
-  const navigation = document.querySelector('nav');
   navigation.classList.toggle('nav-closed');
+});
+
+allItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    navigation.classList.toggle('nav-closed');
+  });
 });
